edca8303b3f74ed195b10d4783f78af0ea63c90a
emilysamantha <emilyzarry@gmail.com>
2022-08-08 14:33:54 +1000

    Add sorting by team1

--- assignment1.py
+++ assignment1.py
@@ -16,7 +16,7 @@ def analyze(results: list, roster: int, score: int) -> list:
 
     TO-DO:
     - [DONE] Take into account alternate score
-    - Check for duplicate matches (matches that have the same team combination and same score)
+    - [DONE] Check for duplicate matches (matches that have the same team combination and same score)
     - Check for results that have less than 10 matches
     """
     # Create top10_matches and searched_matches arrays
@@ -32,14 +32,21 @@ def analyze(results: list, roster: int, score: int) -> list:
     # Sort by team1
     starting_index = 0
     stopping_index = 0
-    for i in range(1, len(sorted_results)):         # O(N) * EXCEED
+    for i in range(1, len(sorted_results)):         # O(N) *
+        # If the current score is equal to the previous score,
+        # set the current index as stopping index
         if sorted_results[i][2] == sorted_results[i - 1][2]:
             stopping_index = i
         # Else if the current score is different from the previous score,
         # sort team1 from the starting_index up to the stopping_index
         elif sorted_results[i][2] != sorted_results[i - 1][2] and starting_index < stopping_index:
-            sorted_results = radix_sort_team1(sorted_results, roster, starting_index, stopping_index + 1)
+            sorted_sublist = radix_sort_team1(sorted_results, roster, starting_index, stopping_index + 1)
+            for j in range(starting_index, stopping_index + 1):
+                sorted_results[j] = sorted_sublist[j - starting_index]
             starting_index = i
+        # Else if the current score is different from the previous score
+        # and starting_index and stopping_index are the same,
+        # set the current index as the starting_index
         else:
             starting_index = i
 
@@ -47,7 +54,7 @@ def analyze(results: list, roster: int, score: int) -> list:
     for i in range(3):                             # O(1)
         top10_matches.append(sorted_results[i])
 
-    # return [top3_matches, searched_matches]
+    # return [top10_matches, searched_matches]
     return top10_matches
 
 
@@ -148,35 +155,32 @@ def counting_sort_string(string: str, roster: int) -> str:
 
 def counting_sort_team1(lst: list, roster: int, char_place: int, starting_index: int, stopping_index: int) -> list:
     # Create count array
-    count = [0 for i in range(roster)]                  # O(1)
+    count = [0 for i in range(roster)]                              # O(1)
 
     # Iterate through each value in the sub-array to be sorted and increment count[char_index] by 1
-    for i in range(starting_index, stopping_index):     # O(N)
+    for i in range(starting_index, stopping_index):                 # In total for all calls: O(N)
         char_processed = lst[i][0][-char_place]
         char_index = ord(char_processed) - 65
         count[char_index] += 1
 
     # Create position array
-    position = [0 for i in range(roster)]               # O(1)
+    position = [0 for i in range(roster)]                           # O(1)
 
     # Iterate through each value in count and set position[i] = position[i-1] + count[i-1]
-    for i in range(1, roster):                          # O(1)
+    for i in range(1, roster):                                      # O(1)
         position[i] = position[i - 1] + count[i - 1]
 
     # Create output array
-    output = [0 for i in range(len(lst))]               # O(N)
+    output = [0 for i in range(starting_index, stopping_index)]     # In total for all calls: O(N)
 
     # Iterate through each value in the sub-array and set output[position[char_index]] = lst[i]
-    for i in range(len(lst)):                           # O(N)
-        if i not in range(starting_index, stopping_index):
-            output[i] = lst[i]
-        else:
-            char_processed = lst[i][0][-char_place]
-            char_index = ord(char_processed) - 65
-            output[position[char_index] + starting_index] = lst[i]
+    for i in range(starting_index, stopping_index):                 # In total for all calls: O(N)
+        char_processed = lst[i][0][-char_place]
+        char_index = ord(char_processed) - 65
+        output[position[char_index]] = lst[i]
 
-            # Increment position[value]
-            position[char_index] += 1
+        # Increment position[value]
+        position[char_index] += 1
 
     return output
 
@@ -187,20 +191,20 @@ def radix_sort_team1(lst: list, roster: int, starting_index: int, stopping_index
         return
 
     # Create result array
-    result = [lst[i] for i in range(len(lst))]  # O(N)
+    output = [lst[i] for i in range(starting_index, stopping_index)]    # In total for all calls: O(N)
 
     # Determine length of team
     team_length = len(lst[0][0])
 
-    # Call countingSortString team_length times, looking at one character at a time starting from the last character
+    # Call counting_sort_team1 team_length times, looking at one character at a time starting from the last character
     char_place = 1
-    for _ in range(team_length):                # O(M) * O(N)
-        result = counting_sort_team1(lst, roster, char_place, starting_index, stopping_index)
+    for _ in range(team_length):                                        # In total for all calls: O(M) * O(N)
+        output = counting_sort_team1(lst, roster, char_place, starting_index, stopping_index)
         char_place += 1
 
-    return result
+    return output
 
-list = [["ABC", "BDD", 85], ["ABC", "BDD", 85], ["EAE", "BCA", 85], ["EEE", "BDB", 17], ["EAD", "ECD", 21],
+list = [["CBA", "DBD", 85], ["ABC", "BDD", 85], ["EAE", "BCA", 85], ["EEE", "BDB", 17], ["EAD", "ECD", 21],
 ["ECA", "CDE", 13], ["CDA", "ABA", 76]]
 print("Before: ")
 print(list)
