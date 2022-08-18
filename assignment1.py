555dfddb524413858b48df4b02e28c16a34039ab
emilysamantha <emilyzarry@gmail.com>
2022-08-18 17:26:54 +1000

    Final (before removing pop)

--- assignment1.py
+++ assignment1.py
@@ -28,29 +28,29 @@ def analyze(results: list, roster: int, score: int) -> list:
              top10matches is a list of 10 matches with the highest score and
              searchedmatches is a list of matches with the same score as score
 
-    :Time Complexity: O(N * M)
-    :Aux Space Complexity: O(N * M)     ??
+    :Time Complexity: O(N * M), where N is the length of results and M is the number of characters in each team
+    :Aux Space Complexity: O(N * M), where N is the length of results and M is the number of characters in each team
     """
     # Create top10_matches and searched_matches arrays
     top10matches = []
     searchedmatches = []
 
     # Go through results and if a score is less than 50, switch it to its alternate format
-    switch_format(results)
+    add_reverse(results)
 
     # Sort the team string in each match
-    for i in range(len(results)):       # O(N) * O(M)
+    for i in range(len(results)):  # O(N) * O(M)
         results[i][0] = counting_sort_string(results[i][0], roster)
         results[i][1] = counting_sort_string(results[i][1], roster)
 
     # Sort team2 in lexicographical order
-    results = radix_sort_team(results, roster, 1)       # O(M) * (N)
+    results = radix_sort_team(results, roster, 1)  # O(M) * (N)
 
     # Sort team1 in lexicographical order
-    results = radix_sort_team(results, roster, 0)       # O(M) * (N)
+    results = radix_sort_team(results, roster, 0)  # O(M) * (N)
 
     # Sort score in descending order
-    results = radix_sort_score(results)                 # O(N)
+    results = radix_sort_score(results)  # O(N)
 
     # Filter duplicate matches
     filter_duplicates(results)
@@ -61,7 +61,7 @@ def analyze(results: list, roster: int, score: int) -> list:
         num_top_matches = len(results)
 
     # Grab top 10 highest matches from sorted results
-    for i in range(num_top_matches):            # O(1)
+    for i in range(num_top_matches):  # O(1)
         top10matches.append(results[i])
 
     # Look for searchedmatches
@@ -70,23 +70,21 @@ def analyze(results: list, roster: int, score: int) -> list:
     return [top10matches, searchedmatches]
 
 
-def switch_format(lst: list) -> None:
+def add_reverse(lst: list) -> None:
     """
-    Function to switch the format of matches that have a score below 50.
+    Function to switch the format of matches.
     Switches team1 with team2 and sets the score of the match as the alternate format (100-score).
 
     :Input:
         lst: The results list
 
-    :Time Complexity: O(N)
+    :Time Complexity: O(N), where N is the length of lst
     :Aux Space Complexity: O(1)
     """
     # Go through each match in the list
     for i in range(len(lst)):  # O(N)
-        # If the score is less than 50
-        if lst[i][2] < 50:
-            # Flip the teams and invert the score
-            lst[i] = [lst[i][1], lst[i][0], 100 - lst[i][2]]
+        # Append the reverse format to the list
+        lst.append([lst[i][1], lst[i][0], 100 - lst[i][2]])
 
 
 def counting_sort_string(string: str, roster: int) -> str:
@@ -103,14 +101,14 @@ def counting_sort_string(string: str, roster: int) -> str:
     :Return:
         str: string that has been sorted in ascending lexicographical order
 
-    :Time Complexity: O(M)
-    :Aux Space Complexity: O(M)
+    :Time Complexity: O(M), where M is the number of characters in the string
+    :Aux Space Complexity: O(M), where M is the number of characters in the string
     """
     # Create count array
-    count = [0 for i in range(roster)]          # O(1)
+    count = [0 for i in range(roster)]  # O(1)
 
     # Go through each character in string and increment count[char_index] by 1
-    for i in range(len(string)):        # O(M)
+    for i in range(len(string)):  # O(M)
         char_processed = string[i]
         char_index = ord(char_processed) - 65
         count[char_index] += 1
@@ -126,7 +124,7 @@ def counting_sort_string(string: str, roster: int) -> str:
             chars_to_join.append(chr(i + 65))
 
     # Return the final output string by using the join method
-    return "".join(chars_to_join)        # O(M)
+    return "".join(chars_to_join)  # O(M)
 
 
 def radix_sort_team(lst: list, roster: int, team_num: int) -> list:
@@ -147,8 +145,8 @@ def radix_sort_team(lst: list, roster: int, team_num: int) -> list:
     :Return:
         lst: The results list that has been sorted
 
-    :Time Complexity: O(M * N)
-    :Aux Space Complexity: O(N)
+    :Time Complexity: O(M * N), where N is the length of lst and M is the number of characters in each team
+    :Aux Space Complexity: O(N), where N is the length of lst
     """
     # Determine number of characters in the team
     if len(lst) == 0:
@@ -158,7 +156,7 @@ def radix_sort_team(lst: list, roster: int, team_num: int) -> list:
 
     # Call counting_sort_team team_length times, looking at one character at a time starting from the last character
     char_place = 1
-    for _ in range(num_chars):                  # O(M) * O(N)
+    for _ in range(num_chars):  # O(M) * O(N)
         lst = counting_sort_team(lst, roster, char_place, team_num)
         char_place += 1
 
@@ -187,30 +185,30 @@ def counting_sort_team(lst: list, roster: int, char_place: int, team_num: int) -
     :Return:
         lst: The results list that has been sorted
 
-    :Time Complexity: O(N)
-    :Aux Space Complexity: O(N)
+    :Time Complexity: O(N), where N is the length of lst
+    :Aux Space Complexity: O(N), where N is the length of lst
     """
     # Create count array
-    count = [0 for i in range(roster)]          # O(1)
+    count = [0 for i in range(roster)]  # O(1)
 
     # Go through each character in lst and increment count[char_index] by 1
-    for i in range(len(lst)):                   # O(N)
+    for i in range(len(lst)):  # O(N)
         char_processed = lst[i][team_num][-char_place]
         char_index = ord(char_processed) - 65
         count[char_index] += 1
 
     # Create position array
-    position = [0 for i in range(roster)]       # O(1)
+    position = [0 for i in range(roster)]  # O(1)
 
     # Go through each value in count and set position[i] = position[i-1] + count[i-1]
-    for i in range(1, roster):                  # O(1)
+    for i in range(1, roster):  # O(1)
         position[i] = position[i - 1] + count[i - 1]
 
     # Create output array
-    output = [0 for i in range(len(lst))]       # O(N)
+    output = [0 for i in range(len(lst))]  # O(N)
 
     # Go through each value in lst and set output[position[char_index]] = lst[i]
-    for i in range(len(lst)):                   # O(N)
+    for i in range(len(lst)):  # O(N)
         char_processed = lst[i][team_num][-char_place]
         char_index = ord(char_processed) - 65
         output[position[char_index]] = lst[i]
@@ -233,12 +231,12 @@ def radix_sort_score(lst: list) -> list:
     :Return:
         lst: The results list that has been sorted
 
-    :Time Complexity: O(N)
-    :Aux Space Complexity: O(N)
+    :Time Complexity: O(N), where N is the length of lst
+    :Aux Space Complexity: O(N), where N is the length of lst
     """
     # Call counting_sort_score 3 times, looking at one digit at a time starting from the least significant digit
     digit_place = 0
-    for i in range(3):                          # O(1) * O(N) = O(N)
+    for i in range(3):  # O(1) * O(N) = O(N)
         lst = counting_sort_score(lst, digit_place)
         digit_place += 1
 
@@ -259,29 +257,29 @@ def counting_sort_score(lst: list, digit_place: int) -> list:
     :Return:
         lst: The results list that has been sorted
 
-    :Time Complexity: O(N)
-    :Aux Space Complexity: O(N)
+    :Time Complexity: O(N), where N is the length of lst
+    :Aux Space Complexity: O(N), where N is the length of lst
     """
     # Create count array
-    count = [0 for i in range(10)]              # O(1)
+    count = [0 for i in range(10)]  # O(1)
 
     # Go through each value in lst and increment count[digit_processed] by 1
-    for i in range(len(lst)):                   # O(N)
+    for i in range(len(lst)):  # O(N)
         digit_processed = (lst[i][2] // (10 ** digit_place)) % 10
         count[digit_processed] += 1
 
     # Create position array
-    position = [0 for i in range(10)]           # O(1)
+    position = [0 for i in range(10)]  # O(1)
 
     # Go through each value in count and set position[i] = position[i-1] + count[i-1]
-    for i in range(8, -1, -1):                  # O(1)
-        position[i] = position[i+1] + count[i+1]
+    for i in range(8, -1, -1):  # O(1)
+        position[i] = position[i + 1] + count[i + 1]
 
     # Create output array
-    output = [0 for i in range(len(lst))]       # O(N)
+    output = [0 for i in range(len(lst))]  # O(N)
 
     # Go through each value in lst and set output[position[digit_processed]] = lst[i]
-    for i in range(len(lst)):                   # O(N)
+    for i in range(len(lst)):  # O(N)
         digit_processed = (lst[i][2] // (10 ** digit_place)) % 10
         output[position[digit_processed]] = lst[i]
 
@@ -298,7 +296,7 @@ def filter_duplicates(lst: list) -> None:
     :Input:
         lst: The results list to be filtered
 
-    :Time Complexity: O(N)
+    :Time Complexity: O(N), where N is the length of lst
     :Aux Space Complexity: O(1)
     """
     # Go through the list and if they have the same team and score, only keep one
@@ -323,81 +321,36 @@ def find_searchedmatches(lst: list, score: int, searchedmatches: list) -> None:
         score: The score we want to search for and return in searchedmatches.
         searchedmatches: list of matches with the same score as score.
 
-    :Time Complexity: O(N)
+    :Time Complexity: O(N), where N is the length of lst
     :Aux Space Complexity: O(1)
     """
     # Create flag to indicate whether a match has been found
     found = False
     next_highest = 101
 
-    # If the score we are searching for is less than 50
-    if score < 50:
-        # Iterate through lst from the smallest index (going through lst from the smallest score going up)
-        for i in range(len(lst)):       # O(N)
-            # Invert the score in lst
-            score_to_check = 100 - lst[i][2]
-
-            # If score_to_check is equal to the score we are searching for
-            if score_to_check == score:
-                # Append the match to searchedmatches, but in the alternate format
-                searchedmatches.append([lst[i][1], lst[i][0], score_to_check])
-                # Mark found as True
-                found = True
-            # Else if we have found an equal score and score_to_check is greater than the score
-            # or if we have not found an equal score (meaning we have appended the next highest score)
-            # and score_to_check is greater than the next highest score
-            elif (found and score_to_check > score) or (not found and score_to_check > next_highest):
-                # Stop searching (break out of the loop)
-                break
-            # Else if we have not found an equal score and score_to_check is greater than the score
-            # we are searching for
-            elif not found and score_to_check > score:
-                # Append the match to searchedmatches, but in the alternate format
-                searchedmatches.append([lst[i][1], lst[i][0], score_to_check])
-                # Set next highest to this score
-                next_highest = score_to_check
-            # ELse if we have not found an equal match and score_to_check is equal to the next highest score
-            elif not found and score_to_check == next_highest:
-                # Append the match to searchedmatches, but in the alternate format
-                searchedmatches.append([lst[i][1], lst[i][0], score_to_check])
-    # Else if the score we are searching for is 50 or greater
-    else:
-        # Going through lst from the largest index (going through lst from the smallest score going up)
-        for i in range(len(lst) - 1, -1, -1):  # O(N)
-            score_to_check = lst[i][2]
-            # If score_to_check is equal to the score we are searching for
-            if score_to_check == score:
-                # Append the match to searchedmatches
-                searchedmatches.insert(0, lst[i])
-                # Mark found as True
-                found = True
-            # ELse if we have found an equal score and score to check is greater than the score
-            # or if we have not found an equal score (meaning we have appended the next highest score)
-            # and score_to_check is greater than the next highest score
-            elif (found and score_to_check > score) or (not found and score_to_check > next_highest):
-                # Stop searching (break out of the loop)
-                break
-            # Else if we have not found an equal score and score_to_check is greater than the score
-            # we are searching for
-            elif not found and score_to_check > score:
-                # Append the match to searchedmatches
-                searchedmatches.insert(0, lst[i])
-                # Set next highest to this score
-                next_highest = score_to_check
-            # ELse if we have not found an equal match and score_to_check is equal to the next highest score
-            elif not found and lst[i][2] == next_highest:
-                # Append the match to searchedmatches
-                searchedmatches.append(lst[i])
-
-
-results = [["AAB", "AAB", 35], ["AAB", "BBA", 49], ["BAB", "BAB", 42],
-           ["AAA", "AAA", 38], ["BAB", "BAB", 36], ["BAB", "BAB", 36],
-           ["ABA", "BBA", 57], ["BBB", "BBA", 32], ["BBA", "BBB", 49],
-           ["BBA", "ABB", 55], ["AAB", "AAA", 58], ["ABA", "AAA", 46],
-           ["ABA", "ABB", 44], ["BBB", "BAB", 32], ["AAA", "AAB", 36],
-           ["ABA", "BBB", 48], ["BBB", "ABA", 33], ["AAB", "BBA", 30],
-           ["ABB", "BBB", 68], ["ABB", "ABB", 68], ["BAB", "BBB", 52]]
-
-results = [['A', 'B', 50]]
-
-print(analyze(results, 2, 100))
+    # Going through lst from the largest index (going through lst from the smallest score going up)
+    for i in range(len(lst) - 1, -1, -1):  # O(N)
+        score_to_check = lst[i][2]
+        # If score_to_check is equal to the score we are searching for
+        if score_to_check == score:
+            # Append the match to searchedmatches
+            searchedmatches.insert(0, lst[i])
+            # Mark found as True
+            found = True
+        # ELse if we have found an equal score and score to check is greater than the score
+        # or if we have not found an equal score (meaning we have appended the next highest score)
+        # and score_to_check is greater than the next highest score
+        elif (found and score_to_check > score) or (not found and score_to_check > next_highest):
+            # Stop searching (break out of the loop)
+            break
+        # Else if we have not found an equal score and score_to_check is greater than the score
+        # we are searching for
+        elif not found and score_to_check > score:
+            # Append the match to searchedmatches
+            searchedmatches.insert(0, lst[i])
+            # Set next highest to this score
+            next_highest = score_to_check
+        # ELse if we have not found an equal match and score_to_check is equal to the next highest score
+        elif not found and lst[i][2] == next_highest:
+            # Append the match to searchedmatches
+            searchedmatches.append(lst[i])
